5ffe3393f2be876acb8661e43dcf03abd3d008a0
Cam Hoang <choang@smsmt.com>
2014-04-16 15:12:34 +0700

    Release

--- FlowChart.js
+++ FlowChart.js
@@ -3,18 +3,20 @@
  * 
  * Copyright (c) 2014, AIWSolutions
  * License: GPL2
- * Project Website: http://wiki.aiwsolutions.net/
+ * Project Website: http://wiki.aiwsolutions.net/PXR4E
  **/
 
 jQuery.fn.flowChart = function(targetDiv) {
 	var source = $(this);
 	var target = $(targetDiv);
 	var elementCount = 0;
-	var arrowElementRatio = 0.3;
+	var arrowElementRatio = 0.5;
 	var arrowBodyRatio = 0.5;
-	var defaultSpace = 10;
+	var arrowBodyHeadRatio = 0.5;
+	var defaultSpace = 20;
 	
 	function renderElements() {
+		target.css('position', 'relative');
 		source.find('li').each(function(index) {
 			if (index > 0) {
 				var arrow_body = $('<div class="arrow-body arrow-' + index + '"></div>');
@@ -22,8 +24,10 @@ jQuery.fn.flowChart = function(targetDiv) {
 				target.append(arrow_body);
 				target.append(arrow_head);
 			}
-			var element = $('<div class="item-' + index + '"></div>');
-			element.append($(this).text());
+			var element = $('<div class="item item-' + (index + 1) + '"></div>');
+			var elementContent = $('<div class="item-content"></div>');
+			elementContent.append($(this).html());
+			element.append(elementContent);
 			target.append(element);
 			elementCount++;
 		});
@@ -33,15 +37,27 @@ jQuery.fn.flowChart = function(targetDiv) {
 		var viewWidth = target.width();
 		var viewHeight = target.height();
 		var elementWidth = (viewWidth - 2 * defaultSpace * (elementCount - 1)) / (elementCount + (elementCount - 1) * arrowElementRatio);
+		var elementHeight = viewHeight;
 		var arrowWidth = elementWidth * arrowElementRatio;
+		var arrowHeight = elementHeight * arrowElementRatio;
 		var arrowBodyWidth = arrowWidth * arrowBodyRatio;
 		var arrowHeadWidth = arrowWidth - arrowBodyWidth;
+		var arrowHeadTop = (viewHeight - arrowHeight) / 2;
+		var arrowBodyHeight = arrowHeight * arrowBodyHeadRatio;
+		var arrowBodyTop = (viewHeight - arrowBodyHeight) / 2;
 		for (var i = 0; i < elementCount; i++) {
-			var itemLeft = i * (elementWidth * (1 + arrowElementRatio) + defaultSpace * 2);
-			$('item-' + i).css('left', itemLeft).
-				css('width', elementWidth);
+			var elementLeft = i * (elementWidth * (1 + arrowElementRatio) + defaultSpace * 2);
+			$('.item-' + (i + 1)).css('left', elementLeft).
+				css('width', elementWidth).css('height', elementHeight).each(function() {
+					var contentElement = $($(this).find('.item-content')[0]);
+					var contentElementLeft = (elementWidth - contentElement.width()) / 2;
+					var contentElementTop = (elementHeight - contentElement.height()) / 2;
+					contentElement.css('left', contentElementLeft).css('top', contentElementTop);
+				});
 			if (i > 0) {
-				$('arrow-body arrow-' + i).css('width', arrowBodyWidth).css('left', i * elementWidth;
+				var arrowLeft = elementLeft - defaultSpace - arrowWidth;
+				$('.arrow-body.arrow-' + i).css('width', arrowBodyWidth).css('left', arrowLeft).css('height', arrowBodyHeight).css('top', arrowBodyTop);
+				$('.arrow-head.arrow-' + i).css('width', 0).css('left', arrowLeft + arrowBodyWidth).css('height', 0).css('top', arrowHeadTop).css('border-width', (arrowHeight / 2) + 'px 0 ' + (arrowHeight / 2) + 'px ' + arrowHeadWidth + 'px');
 			}
 		}
 	}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 flowchart
 =========
 
-Simple jQuery plugin to render a flow chart.
+Simple jQuery plugin to render a flow chart from HTML <ul> and <li> elements.
+
+Please refer to http://wiki.aiwsolutions.net/PXR4E for demonstration and full guidelines.
